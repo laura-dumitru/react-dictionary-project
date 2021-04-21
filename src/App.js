d6b1724e16c5e17d4e599294fbb90a50afe9a0cb
Laura Dumitru <lauradumitru88@icloud.com>
2021-04-21 17:20:10 +0100

    Added search engine
    Added search engine

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header">Hello</header>
+        <main>
+          <Dictionary />
+        </main>
+      </div>
+      <footer className="App-footer">Coded by Laura</footer>
+    </div>
+  );
+}
