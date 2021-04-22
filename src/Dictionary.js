d1428d742c7c45a3ba13468e14bcbba3d6ab7b0e
Laura Dumitru <lauradumitru88@icloud.com>
2021-04-22 21:48:08 +0100

    Rendered word meanings

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results";
 import "./Dictionary.css";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
 
   function search(event) {
     event.preventDefault();
+
+    //documentation: https://dictionaryapi.dev
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
-  //documentation: https://dictionaryapi.dev
-  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-
-  axios.get(apiUrl).then(handleResponse);
-
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
@@ -24,6 +27,7 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input type="search" onChange={handleKeywordChange} />
       </form>
+      <Results results={results} />
     </div>
   );
 }
