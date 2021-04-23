fcd3437dbf29443e164d9ba23f1c86d044084d77
Laura Dumitru <lauradumitru88@icloud.com>
2021-04-23 16:11:46 +0100

    Added link to Git Hub

--- src/App.js
+++ src/App.js
@@ -5,12 +5,18 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">Hello</header>
+        <header className="App-header">What word are you looking for?</header>
         <main>
           <Dictionary />
         </main>
       </div>
-      <footer className="App-footer">Coded by Laura</footer>
+      <footer className="">
+        Coded by
+        <a href="https://github.com/laura-dumitru/react-dictionary-project">
+          {" "}
+          Laura{" "}
+        </a>
+      </footer>
     </div>
   );
 }
