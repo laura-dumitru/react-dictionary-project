c34147bb00d5acad30910e0328bb681ad631d37c
Laura Dumitru <lauradumitru88@icloud.com>
2021-04-26 15:25:03 +0100

    Added Synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
@@ -8,9 +9,14 @@ export default function Meaning(props) {
         return (
           <div key={index}>
             <p>
+              <strong>Definition:</strong>
               {definition.definition}
               <br />
+              <strong>Example: </strong>
               <em>{definition.example}</em>
+              {definition.synonyms}
+
+              <Synonyms synonyms={definition.synonyms} />
             </p>
           </div>
         );
